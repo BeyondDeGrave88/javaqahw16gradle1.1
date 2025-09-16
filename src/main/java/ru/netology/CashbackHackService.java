685c03e132d45c4731466d58e8a309cd76ea501f
Dmitrij Sokolov <darkfalken88@gmail.com>
2025-09-16 13:37:56 +0300

    CashbackHackService downgraded

--- src/main/java/ru/netology/CashbackHackService.java
+++ src/main/java/ru/netology/CashbackHackService.java
@@ -4,13 +4,6 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        if (amount < 0) {
-            throw new IllegalArgumentException("Сумма покупки не может быть отрицательной");
-        }
-        if (amount == 0) {
-            return boundary;
-        }
-        int remainder = amount % boundary;
-        return remainder == 0 ? 0 : boundary - remainder;
+        return boundary - amount % boundary;
     }
 }
